fe84fe3f13e8cc50b7471e5e9dc3092099db7be3
GillyMalova <malovagilbert@gmail.com>
2022-07-09 20:51:08 +0200

    player selection set to case insensitive

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         return 'You lose! Paper beats Rock'
-    }else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    }else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
         return 'You win! Rock beats Scissors'
-    }else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+    }else if (playerSelection === 'paper' && computerSelection === 'Rock') {
         return 'You win! Paper beats Rock'
-    }else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+    }else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
         return 'You lose! Scissors beat Paper'
-    }else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+    }else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
         return 'You win! Scissor beats paper'
-    }else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    }else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
         return 'You lose! Rock beats Scissors'
     } else return 'draw';
 }
-const playerSelection = prompt('Rock, Paper or scissors?');
+const playerSelection = prompt('Rock, Paper or scissors?').toLowerCase();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
