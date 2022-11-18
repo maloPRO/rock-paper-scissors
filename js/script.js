8ee18e87c0a2ccb0d1de8dee670bf64380e6c053
maloPRO <malovagilbert@gmail.com>
2022-11-19 01:43:44 +0300

    added game over screen and new game function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 const gameOptions = ['rock', 'paper', 'scissors'];
+const pScore = document.getElementById('playerScore');
+const cScore = document.getElementById('computerScore');
+const result = document.getElementById('results');
+const gameScreen = document.getElementById('gameScreen');
+const container = document.getElementById('container');
+const gameOverScreen = document.createElement('div');
+const winner = document.createElement('div')
+const newGame = document.createElement('button');
+
+gameOverScreen.setAttribute('id', 'gameOverScreen');
+winner.setAttribute('id', 'winner');
+newGame.setAttribute('id', 'newGame');
+newGame.textContent = 'New Game';
+
+gameOverScreen.appendChild(winner)
+gameOverScreen.appendChild(newGame);
+
+
 function computerPlay() {
     return gameOptions[Math.floor(Math.random()* 3)];
 }
@@ -7,76 +25,69 @@ let computerScore = 0;
 let playerScore = 0;
 let roundWinner = '';
 
-function  playRound(playerSelection) {
+function gameOver() {
+	container.removeChild(gameScreen)
+	container.appendChild(gameOverScreen);
 
-    function gameOver() {
-        playerScore = 0;
-        computerScore = 0;
-        result.textContent = 'Game over.'
-    }
+	pScore.textContent = 'Player Score: ' + 0; 
+	cScore.textContent = 'Computer Score: ' + 0;
 
-    let computerSelection = computerPlay();
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+	result.textContent = 'Select an option to Start';
+}
+
+const  playRound = (playerSelection, computerSelection) => {
+
+  computerSelection = computerPlay();
+
+  if (playerSelection === 'rock' && computerSelection === 'scissors') {
     playerScore++;
     roundWinner = 'player';
+  }else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    playerScore++;
+    roundWinner = 'player';
+  }else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    playerScore++;
+    roundWinner = 'player';
+  }else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    computerScore ++;
+    roundWinner = 'computer';
+  }else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    computerScore++;
+    roundWinner = 'computer';
+  }else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    computerScore++;
+    roundWinner = 'computer';
+  }else if (playerSelection === computerSelection) {
+    roundWinner = 'Draw';
+  }
 
-    }else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        roundWinner = 'player';
+  pScore.textContent = 'Player Score: ' + playerScore;    
+  cScore.textContent = 'Computer Score: ' + computerScore;
 
-    }else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++;
-        roundWinner = 'player';
- 
-    }else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        computerScore ++;
-        roundWinner = 'computer';
+  
+  if (roundWinner === 'Draw') {
+    result.textContent = 'TIE!'
+  }else if (roundWinner === 'player') {
+    result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+  }else if (roundWinner === 'computer') {
+    result.textContent = `Computer wins! ${computerSelection} beats ${playerSelection}.`;
+  }else {
+    result.textContent = roundWinner.toUpperCase() + ' WINS!';
+  }
 
-    }else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        computerScore++;
-        roundWinner = 'computer';
-
-    }else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        computerScore++;
-        roundWinner = 'computer';
-
-    }else if (playerSelection === computerSelection) {
-        roundWinner = 'Draw';
-    }
-    const pScore = document.getElementById('playerScore');
-    pScore.textContent = 'Player Score: ' + playerScore;
-
-    const cScore = document.getElementById('computerScore');
-    cScore.textContent = 'Computer Score: ' + computerScore;
-
-    const result = document.getElementById('results');
-    if (roundWinner === 'Draw') {
-        result.textContent = 'TIE!'
-    }else if (roundWinner === 'player') {
-        result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
-    }else if (roundWinner === 'computer') {
-        result.textContent = `Computer wins! ${computerSelection} beats ${playerSelection}.`;
-    }else {
-        result.textContent = roundWinner.toUpperCase() + ' WINS!';
-    }
-
-    if (playerScore === 5) {
-        gameOver();
-        confirm('PLAYER WINS THE GAME.');
-        confirm('Play Again?');
-
-    }else if (computerScore === 5) {
-        gameOver();
-        confirm('COMPUTER WINS THE GAME.');
-        confirm('Play Again?');
-    }
+  if (playerScore === 5) {
+    gameOver();
+		winner.textContent = 'PLAYER WINS'
+  }else if (computerScore === 5) {
+    gameOver();
+    winner.textContent = 'COMPUTER WINS';
+  }
 }
 
 const btn = document.querySelectorAll('button');
 btn.forEach(button => {
     button.addEventListener('click', () => {
         let playerSelection = button.id;
-        let computerSelection = computerPlay();
         playRound(playerSelection);
 
     })
@@ -88,7 +99,10 @@ btn.forEach(button => {
     })
 })
 
-
+newGame.addEventListener('click', () => {
+	container.removeChild(gameOverScreen);
+	container.appendChild(gameScreen)
+})
 
 
 
